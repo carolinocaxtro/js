fb14b1bd8541ff9cf659c0322bf615e75b1fb423
carolino <carolino1699@gmail.com>
2024-01-12 11:40:24 -0300

    first commit

--- hospedagem/urls.py
+++ hospedagem/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import Deletar, Criar, Listar, Detalhar, Editar
 
 urlpatterns = [
-    path('', Listar.as_view(), name='listar_cliente.html'),
-    path('deletar/', Deletar.as_view(), name='deletar_cliente.html'),
-    path('editar/', Editar.as_view(), name='editar_cliente.html'),
+    path('listar/', Listar.as_view(), name='listar_cliente'),
+    path('deletar/<int:pk>/', Deletar.as_view(), name='deletar_cliente'),
+    path('editar/<int:pk>/', Editar.as_view(), name='editar_cliente'),
     path('detalhar/', Detalhar.as_view(), name='detalhar_cliente.html'),
-    path('criar/', Criar.as_view(), name='criar_cliente.html'),
+    path('criar/', Criar.as_view(), name='criar_cliente'),
 ]
--- hospedagem/views.py
+++ hospedagem/views.py
@@ -2,33 +2,46 @@ from django.shortcuts import render
 from django.views.generic import DetailView, UpdateView, ListView, DeleteView, CreateView
 from hospedagem.models import Cliente
 from .forms import ClienteForm, IDClienteForm
-
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 class Detalhar(DetailView):
     model = Cliente
-    template_name = 'detalhar_template'
+    template_name = 'listar_cliente.html'
+
 
 class Listar(ListView):
     model = Cliente
-    template_name = 'listar_template'
-    form_class = ClienteForm
+    template_name = 'listar_clientes.html'
+    context_object_name = 'clientes'
 
+    
+    
 
-class Editar(UpdateView):
+    
+class Editar(UserPassesTestMixin,UpdateView):
     model = Cliente
-    template_name = 'editar_template'
+    form_class = ClienteForm
+    template_name = 'editar_cliente.html'
+    
+    def test_func(self):
+        return self.request.user.is_superuser
+    
 
 
 class Deletar(DeleteView):
     model = Cliente
-    template_name = 'deletar_template'
+    template_name = 'deletar_template.html'
+    success_url = reverse_lazy('listar_cliente') 
 
 
 class Criar(CreateView):
     model = Cliente
-    template_name = 'criar_cliente'
+    template_name = 'criar_cliente.html'
     form_class = ClienteForm
 
+def test_func(self):
+        return self.request.user.is_superuser
 
 
 
